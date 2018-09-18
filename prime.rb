f4f7fc1bcfa293a660bdba5b4ab308579ad5b2fa
Robin Tram <robin.tram@gmail.com>
2018-09-18 19:15:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(integer)
+  new_array = []
+  array = *(1..integer)
+  array.each do |n|
+    new_array.push(integer % n)
+  end 
+  if new_array.count(0) > 2
+    return false 
+  elsif integer <= 1 
+    return false 
+  else 
+    return true
+  end 
+end 
