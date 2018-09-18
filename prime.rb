37ad84b73b57f2ec9af3ef2e74e60e5684d53d94
Robin Tram <robin.tram@gmail.com>
2018-09-18 19:09:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(integer)
   array.each do |n|
     new_array.push(integer % n)
   end 
-  if new_array.count 0 > 1  
+  if new_array.count 0 > 2
     return false 
   else 
     return true
