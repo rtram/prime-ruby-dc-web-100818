9de6c6b7616e24a0c484f48d508e158e746bf5f1
Robin Tram <robin.tram@gmail.com>
2018-09-18 18:48:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+  new_array = []
+  array = [2...integer]
+  array.each do |n|
+    new_array.push(integer % n)
+  end 
+  if new_array.count 0 >= 1  
+    return false 
+  else 
+    return true 
+end 
